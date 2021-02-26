39de5370d0d85fae2e16ee6b5e2353e6155f0853
Chase McDonald <1chasemcd@gmail.com>
2021-02-26 14:09:23 -0800

    updates

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,5 +6,13 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1
-echo "---" | tee -a /tmp/polybar1.log
-polybar -q bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "---" | tee -a /tmp/polybar1.log
+# polybar -q bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
